267feba3d5449154ae9e34833957d65c870a47af
John Smith <example@standford.edu>
2013-10-12 12:32:23 +0200

    testing my code

--- web.js
+++ web.js
@@ -2,8 +2,14 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var fs = require('fs');
+var file = fs.readFileSync('index.html');
+var buffer = new Buffer(16);
+var text = buffer.toString('utf8', file);
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
